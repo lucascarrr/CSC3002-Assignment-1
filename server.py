853e595f20686134fb497183ba68b270342ddf76
Lucas <crrluc003@myuct.ac.za>
2022-03-02 19:30:59 +0200

    Removed client.py, added code which shows current users to server.py

--- server.py
+++ server.py
@@ -2,35 +2,32 @@ from http import client
 from socket import *
 from User import *
 
+def print_all_users():
+    print ("Current users: ")
+    for users in user_database:
+        users.printDetails()
+    print()
+
 server_ip = '127.0.0.1'
 server_port = 12000
 serverSocket=socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind((server_ip, server_port))
 
 user_database = []
+connections_counter = 0
 
 print("Server Running")
+while True:
+    message,client_address = serverSocket.recvfrom(2048)
 
-message,client_address = serverSocket.recvfrom(2048)
+    x = User(str(client_address[0]), str(client_address[1]), message)
+    user_database.append(x)
 
-x = User(str(client_address[0]), str(client_address[1]), message)
-user_database.append(x)
-
-user_database[0].printDetails()
-
-# outputname = x.name
-# output_ip = x.ip_address
-# output_port = x.port_no
-
-# print (outputname.decode())
-# print (output_ip)
-# print (output_port)
+    user_database[connections_counter].printDetails()
+    connections_counter += 1
+    
+    print_all_users()
 
 
-
-#print (x.name.decode() + " " + x.ip_address.decode())
-
-# print (message.decode())
-# print (str(client_address[0]))
 
 
