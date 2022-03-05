0474addebdbac00facdf46b38cb5f4ab082ae7ce
Lucas <crrluc003@myuct.ac.za>
2022-03-05 21:53:38 +0200

    Revert "Update main.py"
    This reverts commit 49641c480904be2916f15a5ce10d8d99f767fb03.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import threading
 from socket import *
    
-received=False
 def send_message(message):      #send message to the server
     client_socket.sendto(message.encode('utf-8'), (server_ip, server_port))
 
@@ -11,12 +10,9 @@ def send_messages():
 
 def recieve_messages(client_socket):
     while True:
-        try:
-            message, server_address = client_socket.recvfrom(2048)
-            received=True
-            print(message.decode())
-        except:
-            print ("Error receiving message")
+        message, server_address = client_socket.recvfrom(2048)
+        print(message.decode())
+
 
 if __name__ == '__main__':
     #Server details
